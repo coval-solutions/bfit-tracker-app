2fcdee07bda55e2f17ca5237dd87d17bd215444f
DiogoTheCoder <hello@diogothecoder.com>
2019-08-10 11:35:45 +0100

    On location denied, close the app

--- lib/repos/location_repository.dart
+++ lib/repos/location_repository.dart
@@ -1,12 +1,17 @@
+import 'package:flutter/services.dart';
 import 'package:geolocator/geolocator.dart';
 
 class LocationRepository {
   static const String GOOGLE_MAPS_API_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
-  Position _location;
 
   Future<Position> getCurrentLocation() async {
-    _location = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    
-    return _location;
+    Position location =
+      await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+        .catchError(
+          (error) => SystemNavigator.pop()
+        );
+
+    return location;
   }
 }
