c00684afe9bcd036ae7c13b07bb45064d1b07071
Diogo Costa <hello@diogothecoder.com>
2019-11-24 20:29:04 +0000

    Update last goal with daily steps

--- lib/ui/home/profile_area.dart
+++ lib/ui/home/profile_area.dart
@@ -103,7 +103,7 @@ class _ProfileAreaState extends State<ProfileArea> {
                                   child: Container(child: otherStats(snapshot.data.getCourses(), 'Course Total')),
                                 ),
                                 Expanded(
-                                  child: Container(child: otherStats(snapshot.data.getGym(), 'Gym Goal')),
+                                  child: Container(child: otherStats('5032', 'Daily Steps')),
                                 ),
                               ],
                             ),                          
