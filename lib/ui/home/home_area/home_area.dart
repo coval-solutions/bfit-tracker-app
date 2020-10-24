689e7e3dd0bd42aaf56b5f69017b5f268474f49d
Diogo Costa <hello@diogothecoder.com>
2020-10-24 12:55:16 +0100

    BF-21: allow user to refresh the Near Gyms card, and some refactoring (#41)

--- lib/ui/home/home_area/home_area.dart
+++ lib/ui/home/home_area/home_area.dart
@@ -2,11 +2,10 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:bfit_tracker/blocs/authentication/authentication_bloc.dart';
 import 'package:bfit_tracker/blocs/gym/gym_bloc.dart';
 import 'package:bfit_tracker/blocs/location/location_bloc.dart';
-import 'package:bfit_tracker/controllers/gym_controller.dart';
 import 'package:bfit_tracker/models/coval_user.dart';
-import 'package:bfit_tracker/models/gym.dart';
 import 'package:bfit_tracker/theme.dart';
 import 'package:bfit_tracker/ui/coval_solutions/empty_app_bar.dart';
+import 'package:bfit_tracker/ui/home/home_area/gyms_near_me.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -271,6 +270,11 @@ class _NearByGymsCardState extends State<NearByGymsCard> {
     super.initState();
   }
 
+  Future<void> _refresh() {
+    BlocProvider.of<GymBloc>(context)..add(LoadGym());
+    return Future.value();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -279,98 +283,56 @@ class _NearByGymsCardState extends State<NearByGymsCard> {
         borderRadius: BorderRadius.circular(12.0),
       ),
       color: Colors.white,
-      child: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 12,
-            ),
-            child: Container(
-              child: AutoSizeText(
-                "Gyms Near Me",
-                maxLines: 1,
-                minFontSize: 18,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
+      child: RefreshIndicator(
+        onRefresh: this._refresh,
+        child: LayoutBuilder(
+          builder: (context, constraints) => SingleChildScrollView(
+            physics: AlwaysScrollableScrollPhysics(),
+            child: SizedBox(
+              height: constraints.maxHeight,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 12,
+                    ),
+                    child: Container(
+                      child: AutoSizeText(
+                        "Gyms Near Me",
+                        maxLines: 1,
+                        minFontSize: 18,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: BlocConsumer<LocationBloc, LocationState>(
+                      listener: (BuildContext context, state) {},
+                      builder: (BuildContext context, state) {
+                        if (state is LocationError) {
+                          return Center(
+                            child: AutoSizeText(
+                              'BFit Tracker is unable to locate your current position',
+                              textAlign: TextAlign.center,
+                            ),
+                          );
+                        }
+
+                        if (state is Located) {
+                          return GymsNearMe();
+                        } else {
+                          return Center(child: CircularProgressIndicator());
+                        }
+                      },
+                    ),
+                  )
+                ],
               ),
             ),
           ),
-          Expanded(
-            child: BlocConsumer<LocationBloc, LocationState>(
-              listener: (BuildContext context, state) {},
-              builder: (BuildContext context, state) {
-                if (state is LocationError) {
-                  return Center(
-                    child: AutoSizeText(
-                      'BFit Tracker is unable to locate your current position. 😞',
-                      textAlign: TextAlign.center,
-                    ),
-                  );
-                }
-
-                if (!(state is Located)) {
-                  return Center(child: CircularProgressIndicator());
-                }
-
-                return BlocConsumer<GymBloc, GymState>(
-                  listener: (BuildContext context, state) {},
-                  builder: (BuildContext context, state) {
-                    if (!(state is GymLoaded)) {
-                      return Center(child: CircularProgressIndicator());
-                    }
-
-                    return FutureBuilder<List<Gym>>(
-                      future: context.bloc<GymBloc>().state.props.first,
-                      builder: (BuildContext context, AsyncSnapshot snapshot) {
-                        switch (snapshot.connectionState) {
-                          case ConnectionState.none:
-                          case ConnectionState.waiting:
-                            return Center(child: CircularProgressIndicator());
-                          default:
-                            if (snapshot.hasError) {
-                              return Center(
-                                  child:
-                                      AutoSizeText('Error: ${snapshot.error}'));
-                            } else if (snapshot.data == null) {
-                              return Center(child: CircularProgressIndicator());
-                            } else {
-                              return ListView.separated(
-                                  shrinkWrap: true,
-                                  physics: ClampingScrollPhysics(),
-                                  itemCount: snapshot.data.length,
-                                  itemBuilder: (context, index) {
-                                    return ListTile(
-                                        title: AutoSizeText(
-                                          '${snapshot.data[index].name}',
-                                          maxLines: 1,
-                                          maxFontSize: 14,
-                                        ),
-                                        subtitle: AutoSizeText(
-                                          '${snapshot.data[index].address}',
-                                          maxLines: 1,
-                                          minFontSize: 8,
-                                          maxFontSize: 10,
-                                        ),
-                                        onTap: () {
-                                          GymController.launchGoogleMaps(
-                                              snapshot.data[index].lat,
-                                              snapshot.data[index].lng);
-                                        });
-                                  },
-                                  separatorBuilder: (context, index) {
-                                    return Divider(height: 1);
-                                  });
-                            }
-                        }
-                      },
-                    );
-                  },
-                );
-              },
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
