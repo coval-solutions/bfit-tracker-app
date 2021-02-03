af360765fa238fb0191be7c8ea0859e530ca6455
Diogo Costa <hello@diogothecoder.com>
2021-02-03 17:07:17 +0000

    BF-56: added Webpack config

---
+++ functions/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './src/index.ts',
+  target: 'node',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'lib'),
+  },
+};
